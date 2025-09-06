4dc31654e6724704213d45ed8e72acf2c97e93ac
Neil Okikiolu <neil@okikiolu.com>
2025-09-05 21:17:03 -0600

    chore: remove debug logs

--- cmd/gitws/main.go
+++ cmd/gitws/main.go
@@ -10,13 +10,8 @@ import (
 var version = "dev"
 
 func main() {
-	fmt.Fprintf(os.Stderr, "DEBUG: gitws starting, version: %s\n", version)
-	fmt.Fprintf(os.Stderr, "DEBUG: args: %v\n", os.Args)
-
 	if err := cli.Execute(version); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
-
-	fmt.Fprintf(os.Stderr, "DEBUG: gitws completed successfully\n")
 }
